68ed33eca7e03c477f52a4a3a7bae39eaad2e586
Khurram Shafique <kshafique@shipces.com>
2026-04-29 23:55:09 -0400

    UI: Modernize application with WPF UI library

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace KhurramAudioRoute;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : Wpf.Ui.Controls.FluentWindow
 {
     public MainWindow()
     {
